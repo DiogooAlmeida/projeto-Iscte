8e3ba40be08f0f3b046a3bc9a258de0b5052bb51
FilipeeCosta <fmcas2@iscte-iul.pt>
2024-05-31 23:51:32 +0100

    mudei o o url do login, fogot passowrd e register

--- hackproof-main/hackproof/polls/urls.py
+++ hackproof-main/hackproof/polls/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
-    path('login/register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('tables/', views.tables, name='tables'),
     path('dicas/', views.dicas, name='dicas'),
-    path('password/', views.password, name='password'),
+    path('forgot-password/', views.password, name='password'),
     path('charts/', views.charts, name='charts'),
     path('401/', views.page_401, name='error_401'),
     path('404/', views.page_404, name='error_404'),

5b2d22697e4f410f9b30168d2108bf5b63d66a8a
Polareti <draao@iscte-iul.pt>
2024-06-06 23:30:33 +0100

    email funcional

--- hackproof-main/hackproof/hackproof/settings.py
+++ hackproof-main/hackproof/hackproof/settings.py
@@ -150,5 +150,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "th3f0x2004@gmail.com"
-EMAIL_HOST_PASSWORD = "#"
+EMAIL_HOST_USER = "letter.1alert@gmail.com"
+EMAIL_HOST_PASSWORD = "amgc pcvu rtot pfld"

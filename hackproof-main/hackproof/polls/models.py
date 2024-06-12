8e265208dec8d33d53814a62ea4eea9cb195de10
Rui Quental <ruiaqf@gmail.com>
2024-06-12 19:09:15 +0100

    feat: Add saved_month field to EncryptedFile model

--- hackproof-main/hackproof/polls/models.py
+++ hackproof-main/hackproof/polls/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Path(models.Model):
     path = models.CharField(max_length=500)
+
+class LogFilesEncrypted(models.Model):
+    data = models.TextField()
+    filename = models.CharField(max_length=255)
+    saved_month = models.DateTimeField(auto_now_add=True)
